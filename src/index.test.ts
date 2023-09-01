804eec4423162fec177ad2ddb95777f25128a6e2
Mark <mark@remarkablemark.org>
2023-08-31 23:18:55 -0400

    test: add tests and snapshots

---
+++ src/index.test.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import { exec } from './utils';
+
+const cwd = path.resolve(__dirname, '../lib');
+
+function readFileSync(filepath: string) {
+  return fs.readFileSync(path.resolve(cwd, filepath), {
+    encoding: 'utf8',
+  });
+}
+
+function clean() {
+  fs.rmSync(cwd, { force: true, recursive: true });
+}
+
+describe('CLI', () => {
+  beforeAll(() => {
+    clean();
+    exec('npm run build');
+    exec('git init', { cwd });
+    exec('npm init --yes', { cwd });
+    exec('node index.js', { cwd });
+  });
+
+  afterAll(() => {
+    clean();
+  });
+
+  it('updates package.json', () => {
+    expect(JSON.parse(readFileSync('package.json'))).toMatchSnapshot();
+  });
+
+  it('copies .commitlintrc.json', () => {
+    expect(JSON.parse(readFileSync('.commitlintrc.json'))).toMatchSnapshot();
+  });
+
+  it('adds .husky/.commit-msg', () => {
+    expect(readFileSync('.husky/commit-msg')).toMatchSnapshot();
+  });
+
+  it('commits to git', () => {
+    expect(
+      exec('git --no-pager log -1', { cwd, stdio: 'pipe' }).toString(),
+    ).toContain('chore: set up conventional release');
+  });
+});
